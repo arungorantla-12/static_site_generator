a29c2a6a7311fd46a174d83f015ae80bda15a70f
Arun Gorantla <jr.arun12@gmail.com>
2026-06-27 08:09:27 +0530

    added testclass

---
+++ src/test_textnode.py
@@ -0,0 +1,36 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq2(self):
+        node = TextNode("This is a text node", TextType.IMAGE)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node,node2)
+
+    def test_eq3(self):
+        node = TextNode("This is a text node", TextType.BOLD,"www.bold.com")
+        node2 = TextNode("This is a text node", TextType.BOLD,"www.bold.com")
+        self.assertEqual(node,node2)
+
+    def test_eq4(self):
+        node = TextNode("This is a text node", TextType.BOLD,"www.bold.com")
+        node2 = TextNode("This is a text node", TextType.BOLD,"www.boold.com")
+        self.assertNotEqual(node,node2)
+
+    def test_eq5(self):
+        node = TextNode("This is a text node", TextType.BOLD,"www.bold.com")
+        node2 = TextNode("This is a text node", TextType.IMAGE,"www.bold.com")
+        self.assertNotEqual(node,node2)
+
+    def test_eq6(self):
+        node = TextNode("This is is", TextType.BOLD,"www.bold.com")
+        node2 = TextNode("This is a text node", TextType.BOLD,"www.bold.com")
+        self.assertNotEqual(node,node2)
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -16,7 +16,7 @@ class TextNode():
         self.url = url
 
     def __repr__(self):
-        return f"TextNode({self.text},{self.text_type.value},{self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
     def __eq__(self, other):
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
